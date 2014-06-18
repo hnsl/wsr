841f257aa0b658216a464288dcfe9d09ddd87206
Simon Lindholm <simon.lindholm10@gmail.com>
2014-06-18 20:02:39 +0200

    wsr: add a JSON example

--- examples/hello/src/hello.c
+++ examples/hello/src/hello.c
@@ -1,8 +1,11 @@
 #include "rcd.h"
+#include "json.h"
 #include "wsr.h"
 #include "wsr-mime.h"
 
 #pragma librcd
+
+fstr_t web_root;
 
 typedef struct chat_participant {
     fstr_t name;
@@ -101,6 +104,75 @@ fiber_main chat_fiber(fiber_main_attr) {
     auto_accept_join(chat_send, chat_connect, chat_disconnect, join_server_params, &chat);
 }
 
+static void ws_fail(rcd_fid_t writer_fid, fstr_t msg) {
+    wsr_web_socket_close(1008, msg, writer_fid);
+    sub_heap_e(throw(concs("failed web socket: ", msg), exception_io));
+}
+
+static void frobnicate(json_value_t* val) {
+    switch (val->type) {
+        case JSON_NULL: {
+            val->type = JSON_STRING;
+            val->string_value = "FILE_NOT_FOUND";
+            break;
+        }
+        case JSON_BOOL: {
+            val->bool_value = !val->bool_value;
+            break;
+        }
+        case JSON_NUMBER: {
+            val->number_value = -val->number_value;
+            break;
+        }
+        case JSON_STRING: {
+            fstr_t out = fss(fstr_cpy(val->string_value));
+            for (int i = 0; i < out.len; i++) {
+                uint8_t c = out.str[i];
+                if ((97 <= c && c < 110) || (65 <= c && c < 78)) out.str[i] += 13;
+                else if ((110 <= c && c < 123) || (78 <= c && c < 91)) out.str[i] -= 13;
+            }
+            val->string_value = out;
+            break;
+        }
+        case JSON_ARRAY: {
+            list(json_value_t)* out = new_list(json_value_t);
+            list_foreach(val->array_value, json_value_t, v) {
+                frobnicate(&v);
+                list_push_start(out, json_value_t, v);
+            }
+            val->array_value = out;
+            break;
+        }
+        case JSON_OBJECT: {
+            dict_foreach(val->object_value, json_value_t, k, v) {
+                frobnicate(&v);
+                dict_foreach_replace_current(json_value_t, v);
+            }
+            break;
+        }
+    }
+}
+
+static void ws_echo_json(rio_in_addr4_t peer, rcd_fid_t reader_fid, rcd_fid_t writer_fid, void* cb_arg) {
+    for (;;) { sub_heap {
+        bool binary;
+        fstr_t msg = fss(wsr_web_socket_read(0x10000, reader_fid, &binary));
+        if (binary)
+            ws_fail(writer_fid, "expected text message, got binary");
+        sub_heap {
+            fstr_t res;
+            try {
+                json_tree_t* tree = json_parse(msg);
+                frobnicate(&tree->value);
+                fstr_t resp = fss(json_stringify(tree->value));
+                wsr_web_socket_write(resp, false, writer_fid);
+            } catch (exception_arg, e) {
+                wsr_web_socket_write("failure", false, writer_fid);
+            }
+        }
+    }}
+}
+
 static void ws_echo(rio_in_addr4_t peer, rcd_fid_t reader_fid, rcd_fid_t writer_fid, void* cb_arg) {
     for (;;) { sub_heap {
         bool binary;
@@ -116,12 +188,22 @@ static wsr_rsp_t http_request_cb(wsr_req_t req, void* cb_arg) {
         return wsr_response_static(HTTP_OK, "<h1>this is a lol page</h1>", wsr_mime_txt);
     if (fstr_equal(req.path, "/echo"))
         return wsr_response_web_socket(req, ws_echo, "", cb_arg);
+    if (fstr_equal(req.path, "/echo_json"))
+        return wsr_response_web_socket(req, ws_echo_json, "", cb_arg);
     if (fstr_equal(req.path, "/chat"))
         return wsr_response_web_socket(req, ws_chat, "", cb_arg);
-    return wsr_response_file(req, "/home/simon/p/hello/build");
+    return wsr_response_file(req, web_root);
 }
 
 void rcd_main(list(fstr_t)* main_args, list(fstr_t)* main_env) {
+    fstr_t dir;
+    if (!list_unpack(main_args, fstr_t, &dir)) {
+        rio_debug("pass a path as first argument\n");
+        lwt_exit(1);
+    }
+    global_heap {
+        web_root = fss(rio_file_real_path(dir));
+    }
     rcd_fid_t chat_fid;
     fmitosis {
         chat_fid = sfid(spawn_fiber(chat_fiber("")));
