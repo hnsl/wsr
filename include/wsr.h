0d5f92becdd180e99eb5b52c12d037eca7bc9afa
Hannes Landeholm <hannes@jumpstarter.io>
2014-09-03 23:06:00 +0200

    revert bad hunk modifying wsr_simple_cookie() decl in 2bb920ff2

--- include/wsr.h
+++ include/wsr.h
@@ -302,7 +302,7 @@ static inline wsr_rsp_t wsr_response_web_socket(wsr_req_t req, wsr_wss_cb_t wss_
 }
 
 /// Creates a simple cookie. To delete a cookie, set the value to an empty string.
-wsr_set_cookie_t wsr_simple_cookie(fstr_t key, fstr_t value, uint128_t expires) {
+static inline wsr_set_cookie_t wsr_simple_cookie(fstr_t key, fstr_t value, uint128_t expires) {
     wsr_set_cookie_t cookie = {
         .name = key,
         .value = value,
